593fc42606d0920641e1f79b85bb45ccdde03818
brunosp-49 <santoserobinho@gmail.com>
2022-02-14 17:09:38 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,9 @@
+
+
+//1-Primeiro será impresso 10 e depois 10,5/*
+//2- A resposta é 10,10,10/*
+//3- No lugar da letra p acho melhor "horaPorDia", e no lugar do t "moneyPorDia"/*
+//Exercícios de escrita de código*/
+//1- a-Ele retornou com "Object", acredito que ele enviou assim porque não tinha nenhum valor
+//1-b agora que pedi para o usuario colocar dados, veio como string pois não converti para outros tipos
+
