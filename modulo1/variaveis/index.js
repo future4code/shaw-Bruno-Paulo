5b54f8615e5401e4425226bf630be5148875a91e
Bruno Siqueira <99160023+brunosp-49@users.noreply.github.com>
2022-02-14 20:59:48 -0300

    Update index.js

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -7,3 +7,27 @@
 //1- a-Ele retornou com "Object", acredito que ele enviou assim porque não tinha nenhum valor
 //1-b agora que pedi para o usuario colocar dados, veio como string pois não converti para outros tipos
 
+var nome;
+var idade;
+prompt(nome,"Qual é seu nome?")
+prompt(idade,"Qual é sua idade?")
+
+console.log ("Olá ",nome,  ", você tem ",idade," anos")
+
+
+console.log(typeof {A,B});
+
+var gostaSorvete = prompt("Você gosta de sorvete?")
+var temCachorro = prompt("Você tem um cachorro?")
+var nomeRonaldo = prompt("Seu nome é Ronaldo?")
+
+console.log("Você gosta de sorvete?",gostaSorvete)
+console.log("Você tem um cachorro?",temCachorro)
+console.log("Seu nome é Ronaldo?",nomeRonaldo)
+
+let a = 10
+let b = 25
+let c = a
+a = b
+b = c
+console.log(a,b)
