2c04e9fb83ca8cfd68a90df1239588fb672641b8
brunosp-49 <santoserobinho@gmail.com>
2022-04-06 17:21:35 -0300

    Final

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -4,12 +4,15 @@ import styled from 'styled-components';
 import TelaInicial from './Components/TelaInicial';
 import Cadastros from './Components/Cadastros';
 import axios from 'axios';
+import Detalhes from './Components/Detalhes';
+import ReactTooltip from 'react-tooltip'
 
 const headers = {
   headers: {
-    Authorization: "Janaylla-Firmino-SS"
+    Authorization: "bruno-siqueira-shaw"
   }
 };
+
 
 const TelaTotal = styled.div`
 display: flex;
@@ -22,10 +25,27 @@ font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida San
 
 const MainContainer = styled.div`
  width: 40vw;
- height: 50vh;
+ min-height: 55vh;
  background-image: radial-gradient(circle at 100% 0%, #cb3288 0, #b82d84 25%, #a4287e 50%, #902378 75%, #7e2071 100%);
  border-radius: 20px;
  box-shadow: 10px 5px 5px black;
+`
+const Liespecial = styled.span`
+display: flex;
+width: 6em;
+justify-content: space-between;
+color: #1c001c;
+padding: 0.5em;
+
+div:hover{
+  font-weight: bold;
+}
+`
+
+const ButtonDel = styled.div`
+button:hover{
+    background-color: #e47c7c;
+}
 `
 
 class App extends React.Component {
@@ -33,12 +53,17 @@ class App extends React.Component {
     tela: "inicial",
     inputEmail: "",
     inputNome: "",
-    users: []
+    users: [],
+    userDetail: [],
+    currentid: ""
 }
+
 
 componentDidMount() {
   this.getAllUser();
-}
+  
+};
+
 
 getAllUser() {
   const url =
@@ -68,14 +93,53 @@ postUser = () => {
     .then((res) => {
       //Pegartodas as Users
       this.getAllUser();
+      alert("Usuário adicionado com sucesso")
       //Deixar o texto do input com um valor vazio
       this.setState({
         inputNome: ""
       });
+      this.setState({
+        inputEmail: ""
+      });
     })
     .catch((err) => {
       //Alertar caso um erro aconteça
-      alert(err.response.data.message);
+      alert("Erro ao criar o usuário");
+    });
+};
+
+DeleteUser = () => {
+  //body é criado com o valor que tem no inputName
+  //Aqui é onde a função de post é chamada
+  //No pot passamos a url, body, e o headers
+  axios
+    .delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${this.state.currentid}`,headers)
+    .then((res) => {
+      //Pegartodas as Users
+      this.getAllUser();
+      alert("Usuario deletado com sucesso")
+      //Deixar o texto do input com um valor vazio
+    })
+    .catch((err) => {
+      //Alertar caso um erro aconteça
+      alert("Erro ao deletar o usuário");
+    });
+};
+
+GetUserById = () => {
+  //body é criado com o valor que tem no inputName
+  //Aqui é onde a função de post é chamada
+  //No pot passamos a url, body, e o headers
+  axios
+    .get(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${this.state.currentid}`,headers)
+    .then((res) => {
+      this.setState({
+        userDetail: res.data
+      });
+    })
+    .catch((err) => {
+      //Alertar caso um erro aconteça
+      alert(err.data);
     });
 };
 
@@ -92,13 +156,51 @@ onChangeEmail = (e) => {
 };
 
 onClickPost = () => {
-  this.postUser()
-  this.setState({inputEmail: ""})
-  this.setState({inputNome: ""})
+  if(this.state.inputEmail.length > 0 && this.state.inputNome.length > 0){
+    let confirm = window.confirm("Tem certeza?")
+    if (confirm == true){
+      this.postUser()
+      this.setState({tela: "cadastros"})
+    }else{
+      alert("Cadastro cancelado com sucesso")
+    }
+  } else{
+    alert("Complete os dados corretamente")
+  }
+};
+
+onClickReturn = () => {
+  this.setState({tela: "inicial"})
+};
+
+onClickReturnUsers = () => {
   this.setState({tela: "cadastros"})
 };
 
+onMouseEnterId = (id) => {
+  this.setState({currentid: id})
+}
+
+onClickDelete = () => {
+  this.DeleteUser()
+  if(this.state.tela === "detalhes"){
+    this.setState({tela: "cadastros"})
+  }
+};
+
+onClickCadastrados = () => {
+  this.setState({tela: "cadastros"})
+}
+
+onClickDetail = () => {
+  this.GetUserById()
+  this.setState({tela: "detalhes"})
+};
+
   render(){
+    const componentsUser = this.state.users.map((user) => {
+      return [<Liespecial><div key={user.id} data-tip="Clique para ver mais informações" onMouseEnter={() => this.onMouseEnterId(user.id)}  onClick={this.onClickDetail}>{user.name}</div><ButtonDel><button data-tip="Excluir" onMouseEnter={() => this.onMouseEnterId(user.id)} onClick={this.onClickDelete}>X</button></ButtonDel><ReactTooltip place="left" type="light" effect="float"/></Liespecial>];
+    });
   return (
     <TelaTotal>
       <MainContainer>
@@ -114,16 +216,28 @@ onClickPost = () => {
         onChangeName = {this.onChangeName}
         onChangeEmail = {this.onChangeEmail}
         onClickPost = {this.onClickPost}
+        onClickCadastrados = {this.onClickCadastrados}
         />
         )
         case 'cadastros':
         return (
         <Cadastros
+        componentsUser = {componentsUser}
         state = {this.state}
         getAllUser = {this.getAllUser}
         postUser = {this.postUser}
+        onClickReturn = {this.onClickReturn}
+        onClickDelete = {this.onClickDelete}
         />
         )
+        case 'detalhes':
+          return(
+            <Detalhes
+            userDetail = {this.state.userDetail}
+            onClickDelete = {this.onClickDelete}
+            onClickReturnUsers = {this.onClickReturnUsers}
+            />
+          )
         default:
         return (
         <div>You are a User.</div>
--- modulo2/apis-labenusers/apis-labenusers/src/Components/TelaInicial.jsx
+++ modulo2/apis-labenusers/apis-labenusers/src/Components/TelaInicial.jsx
@@ -14,7 +14,7 @@ h1{
 -webkit-text-stroke-color: #000;
 margin: 0;
 padding: 0;
-padding-top: 8px;
+padding-top: 28px;
 font-size: 3em;
 }
 
@@ -25,26 +25,45 @@ font-size: 1.5em;
 }
 
 button{
-margin-bottom: 1.5em;
+margin-bottom: 0.5em;
 height: 2.5em;
 font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 font-size: 1em;
+margin-top: 12px;
+cursor: pointer;
 }
 
 button:hover{
-    background-color: #d29ebc;
-}
+    background-color: #d29ebc;}
 `
 const InputEmail = styled.input`
 width: 16em;
 height: 2em;
 border-radius: 5px;
+margin-bottom: 12px;
 `
 
 const InputNome = styled.input`
 width: 16em;
 height: 2em;
 border-radius: 5px;
+`
+const BottomButton = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 3em;
+h2{
+  margin-left: 52%;
+  -webkit-text-stroke-width: 0.4px;
+  -webkit-text-stroke-color: #000;
+}
+h2:hover{
+  font-weight: bold;
+  font-size: 1.522em;
+  cursor: pointer;
+}
 `
 
 class TelaInicial extends React.Component {
@@ -53,9 +72,12 @@ class TelaInicial extends React.Component {
       <MainContainer>
           <h1>Bem-vindo</h1>
           <h3>Faça seu cadastro</h3>
-          <InputEmail type="email" placeholder='Email' onChange={this.props.onChangeEmail}/>
-          <InputNome type="text" placeholder='Nome' onChange={this.props.onChangeName}/>
-          <button onClick={this.props.onClickPost}>ENVIAR</button>
+          <InputEmail type="email" value={this.props.state.inputEmail} placeholder='Email' onChange={this.props.onChangeEmail}/>
+          <InputNome type="text" value={this.props.state.inputNome} placeholder='Nome' onChange={this.props.onChangeName}/>
+          <button onClick={() => this.props.onClickPost()}>ENVIAR</button>
+          <BottomButton>
+            <h2 onClick={this.props.onClickCadastrados}>Ir para cadastrados -{'>'} </h2>
+          </BottomButton>
       </MainContainer>
     );}
   }
