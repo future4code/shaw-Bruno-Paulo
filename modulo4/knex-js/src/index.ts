ac1a6a3069458d44d05f0de8ac98ff36446a1b4b
brunosp-49 <santoserobinho@gmail.com>
2022-06-11 01:50:30 -0300

    final

---
+++ modulo4/knex-js/src/index.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express"
+import app from "./app"
+import connection from "./connection"
+
+// Esse arquivo seria o index.ts
+
+const getActorById = async (id: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+  `)
+
+	return result[0][0]
+}
+
+const getActorByName = async (name: string) : Promise<any> =>{
+    const result = await connection.raw(`
+        SELECT * FROM Actor WHERE name = '${name}'
+    `)
+
+    return result[0][0]
+}
+
+const getQuantByGender = async (gender: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT COUNT(*) FROM Actor WHERE gender = '${gender}'
+    `)
+  
+      return result[0][0]
+  }
+
+// Assim a chamada funciona fora dos endpoints com .then()/.catch
+getActorById("001")
+	.then(result => {
+		console.log(result)
+	})
+	.catch(err => {
+		console.log(err)
+	});
+
+// Assim a chamada funciona fora dos endpoints com await
+(async () => {
+  console.log(await getActorById("001") )
+})()
+
+
+// Ou então podemos chamá-la dentro de um endpoint
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+
+    console.log(await getQuantByGender(id))
+
+    res.send(await getQuantByGender(id))
+  } catch (error) {
+		console.log(error)
+    res.status(500).send("Unexpected error")
+  }
+}) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
