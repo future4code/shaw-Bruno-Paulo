8ca1a3899d1546458f234fa0f42d5fef235693b0
brunosp-49 <santoserobinho@gmail.com>
2022-06-14 21:13:38 -0300

    filtros-ordenacao-paginacao0

---
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -0,0 +1,31 @@
+import { app } from "./app";
+import { getAllRecipes } from "./endpoints/getAllRecipes";
+import {connection} from "./data/connection"
+
+app.get("/recipes", getAllRecipes)
+
+export default async function selectAllUsers():Promise<any> {
+    const result = await connection.raw(`
+       SELECT id, name, email, type
+       FROM aula48_exercicio;
+    `)
+ 
+    return result[0]
+ }
+
+export const getAllUsers = async(req: Request,res: Response): Promise<void> =>{
+    try {
+       const users = await selectAllUsers()
+ 
+       if(!users.length){
+          res.statusCode = 404
+          throw new Error("No recipes found")
+       }
+ 
+       res.status(200).send(users)
+       
+    } catch (error) {
+       console.log(error)
+       res.send(error.message || error.sqlMessage)
+    }
+ }
